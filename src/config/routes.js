9753f132ea70a231b58e552bf8d3ad95e7a6f5e2
Alec Montalbano <alec.montalbano@gmail.com>
2017-08-01 19:41:40 -0400

    added the angular router to the application. Created routes that correspond to respective components.

---
+++ src/config/routes.js
@@ -0,0 +1,33 @@
+/*jshint esversion:6*/
+
+function RouterConfig($routeProvider){
+  $routeProvider
+    .when('/', {
+      template: '<home></home>'
+    })
+    .when('/signup', {
+      template: '<signup></signup>'
+    })
+    .when('/login', {
+      template: '<login></login>'
+    })
+    .when('/profile', {
+      template: '<profile></profile>'
+    })
+    .when('/create', {
+      template: '<post-creator></post-creator>'
+    })
+    .when('/edit/:postId', {
+      template: '<post-editor></post-editor>'
+    })
+    .when('/read/:postId', {
+      template: '<post-reader></post-reader>'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+}
+
+RouterConfig.$inject = ['$routeProvider'];
+
+module.exports = RouterConfig;
